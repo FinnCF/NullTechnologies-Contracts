8d3dc834e91c7c48280722bfb685c9e60dcb7b02
Finn Casey Fierro <73711482+FinnCastro@users.noreply.github.com>
2023-08-11 21:23:31 +0100

    Fixes to stability

--- contracts/ERCKMS1/ERCKMS1.sol
+++ contracts/ERCKMS1/ERCKMS1.sol
@@ -5,27 +5,31 @@ import "../@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title ERCKMS1 - Ethereum RSA Cryptographic Key Management System
- * @dev A contract for managing cryptographic keys using RSA encryption.
+ * @dev A contract for managing cryptographic keys using RSA encryption. It allows for the addition, retrieval, and management of RSA keys tied to Ethereum addresses. It also enables the contract owner to set a fee for adding keys and collect accumulated fees.
  */
 contract ERCKMS1 is Ownable {
+    uint256 public _fee; // Fee required to add a new key
 
-    uint256 public _fee; // The fee required to add a new key
-
+    /**
+     * @dev Initializes the contract with an initial fee and transfers ownership to the provided address.
+     * @param _initialFee The initial fee required to add a new key.
+     * @param _owner The address to which ownership of the contract will be transferred.
+     */
     constructor(uint256 _initialFee, address _owner) {
         _fee = _initialFee;
         transferOwnership(_owner);
     }
 
     /**
-     * @dev Sets a new fee required for adding a new key.
-     * @param newFee The new fee value.
+     * @dev Allows the owner to set a new fee for adding keys.
+     * @param newFee The new fee amount.
      */
     function setFee(uint256 newFee) public onlyOwner {
         _fee = newFee;
     }
 
     /**
-     * @dev Collects accumulated fees and transfers them to the contract owner.
+     * @dev Allows the owner to withdraw the balance accumulated from the fees.
      */
     function collectFees() public onlyOwner {
         uint256 balance = address(this).balance;
@@ -33,37 +37,37 @@ contract ERCKMS1 is Ownable {
         payable(owner()).transfer(balance);
     }
 
+    // Struct representing a cryptographic key.
     struct Key {
         address owner;
-        bytes publicRSAKey;
-        bytes privateRSAKeyEncrypted;
-        bytes16 iv; 
+        string publicKeyBase64;
+        string encryptedPrivateKeyBase64;
+        string ivBase64;
         uint256 blockNumber;
     }
 
-    event KeyMade(address indexed user); // Event emitted when a new key is made
+    event KeyMade(address indexed user); // Event emitted when a new key is made.
 
-    mapping(address => Key[]) public keys; // Mapping to store keys for each user
-
-    Key[] public allKeys; // Array to store all keys added to the system
+    mapping(address => Key[]) public keys; // Mapping of Ethereum addresses to their keys.
+    Key[] public allKeys; // Array of all keys.
 
     /**
-     * @dev Adds a new key to the system.
-     * @param publicRSAKey The public RSA key.
-     * @param privateRSAKeyEncrypted The encrypted private RSA key.
-     * @param iv The initialization vector for encryption.
+     * @dev Allows users to add a new key by paying the required fee.
+     * @param publicKeyBase64 The public key in Base64.
+     * @param encryptedPrivateKeyBase64 The encrypted private key in Base64.
+     * @param ivBase64 The initialization vector in Base64.
      */
     function addKey(
-        bytes memory publicRSAKey,
-        bytes memory privateRSAKeyEncrypted,
-        bytes16 iv 
+        string memory publicKeyBase64,
+        string memory encryptedPrivateKeyBase64,
+        string memory ivBase64
     ) public payable {
         require(msg.value == _fee, "Exact fee not met");
         Key memory newKey = Key(
             msg.sender,
-            publicRSAKey,
-            privateRSAKeyEncrypted,
-            iv, 
+            publicKeyBase64,
+            encryptedPrivateKeyBase64,
+            ivBase64,
             block.number
         );
         keys[msg.sender].push(newKey);
@@ -72,9 +76,9 @@ contract ERCKMS1 is Ownable {
     }
 
     /**
-     * @notice Retrieves the most recent key for the given address.
-     * @param _address The address whose key is to be retrieved.
-     * @return Key The most recent key for the given address.
+     * @dev Returns the latest key for a given address.
+     * @param _address The address for which to return the key.
+     * @return The latest key for the given address.
      */
     function getCurrentKey(address _address) public view returns (Key memory) {
         require(keys[_address].length > 0, "No keys found for the address");
@@ -82,53 +86,52 @@ contract ERCKMS1 is Ownable {
     }
 
     /**
-     * @notice Retrieves all the keys for the given address.
-     * @param _address The address whose keys are to be retrieved.
-     * @return Key[] An array containing all the keys for the given address.
-     */
-    function getKeys(address _address) public view returns (Key[] memory) {
-        return keys[_address];
-    }
-
-    /**
-     * @notice Retrieves all the keys.
-     * @return Key[] An array containing all the keys.
-     */
-    function getAllKeys() public view returns (Key[] memory) {
-        return allKeys;
-    }
-
-    /**
-     * @notice Retrieves the total number of keys.
-     * @return uint256 The total number of keys.
-     */
-    function getAllKeysLength() public view returns (uint256) {
-        return allKeys.length;
-    }
-
-    /**
-     * @notice Retrieves the number of keys for the given address.
-     * @param _address The address whose keys count is to be retrieved.
-     * @return uint256 The number of keys for the given address.
+     * @dev Returns the number of keys for a given user.
+     * @param _address The address for which to return the key count.
+     * @return The number of keys for the given address.
      */
     function getUserKeysLength(address _address) public view returns (uint256) {
         return keys[_address].length;
     }
 
     /**
-     * @dev Provides instructions for encrypting and decrypting the privateRSAKeyEncrypted.
+     * @dev Returns all keys for a given address.
+     * @param _address The address for which to return the keys.
+     * @return An array of keys for the given address.
+     */
+    function getKeys(address _address) public view returns (Key[] memory) {
+        return keys[_address];
+    }
+
+    /**
+     * @dev Returns all keys managed by the contract.
+     * @return An array of all keys.
+     */
+    function getAllKeys() public view returns (Key[] memory) {
+        return allKeys;
+    }
+
+    /**
+     * @dev Returns the total number of keys managed by the contract.
+     * @return The total number of keys.
+     */
+    function getAllKeysLength() public view returns (uint256) {
+        return allKeys.length;
+    }
+
+    /**
+     * @dev Provides instructions for encrypting and decrypting the encryptedPrivateKey.
      * @return Explanation of the encryption and decryption process.
      */    
     function getDecryptionInstructions() public pure returns (string memory) {
         return (
             "ENCRYPTION PROCESS:\n"
-            "1) Key Pair Generation [bytes]: A public-private key pair is generated using the RSASSA-PKCS1-v1_5 algorithm. A 4096-bit modulus and a public exponent of [1, 0, 1] are used, and the hash algorithm is SHA-256.\n"
-            "2) Signature Creation [bytes]: The owner of the key creates a signature by signing the public RSA key (bytes) with their private Ethereum key (bytes).\n"
-            "3) Signature Hashing [bytes]: The signature (bytes) is hashed with the SHA-256 algorithm, resulting in a fixed-size hash value (bytes).\n"
-            "4) Encryption of Private Key [bytes]: Using the hashed signature (bytes) as a secret key, the private RSA key (bytes) is encrypted with AES-256. AES in Cipher Block Chaining (CBC) mode is used, and a random initialization vector (IV) [bytes16] is generated to ensure unique encryption.\n"
-            "5) Decryption of Private Key [bytes]: To decrypt the encrypted private key (bytes), the owner recreates the hash by signing the public RSA key (bytes) and hashing it with SHA-256 again. The encrypted private key (bytes) and the IV (bytes16) are then decrypted using the hashed signature (bytes) and AES-CBC.\n"
-            "This process ensures that only the owner, with the correct private Ethereum key (bytes), can derive the secret key needed for decryption. Standardized algorithms are used, enabling compatibility with various implementations."
+            "1) Key Pair Generation: A public-private key pair is generated using the RSASSA-PKCS1-v1_5 algorithm. A 4096-bit modulus and a public exponent of [1, 0, 1] are used, and the hash algorithm is SHA-256. Keys are then encoded.\n"
+            "2) Signature Creation: The owner of the key creates a signature by signing the public RSA key with their private Ethereum key.\n"
+            "3) Signature Hashing: The signature is hashed with the SHA-256 algorithm, resulting in a fixed-size hash value of 256bits.\n"
+            "4) Encryption of Private Key: Using the hashed signature as a secret key, the private RSA key is encrypted with AES-256. AES in Cipher Block Chaining (CBC) mode is used, and a random initialization vector (IV) is generated to ensure unique encryption.\n"
+            "5) Decryption of Private Key: To decrypt the encrypted private key, the owner recreates the hash by signing the public RSA key and hashing it with SHA-256 again. The encrypted private key is then decrypted using the hashed signature, IV and AES-CBC.\n"
+            "This process ensures that only the owner, with the correct private Ethereum key, can derive the secret key needed for decryption. Standardized algorithms are used, enabling compatibility with various implementations. It is an extension of an ethereum private key to allow for the storage of RSA keys."
         );
     }
-
 }
--- contracts/ERCKMS1/IERCKMS1.sol
+++ contracts/ERCKMS1/IERCKMS1.sol
@@ -4,11 +4,19 @@ pragma solidity ^0.8.0;
 interface IERCKMS1 {
     function setFee(uint256 newFee) external;
     function collectFees() external;
-    function addKey(bytes calldata publicRSAKey, bytes calldata privateRSAKeyEncrypted, bytes16 iv) external payable;
-    function getCurrentKey(address _address) external view returns (address owner, bytes memory publicRSAKey, bytes memory privateRSAKeyEncrypted, bytes16 iv, uint256 blockNumber);
-    function getKeys(address _address) external view returns (address[] memory owners, bytes[] memory publicRSAKeys, bytes[] memory privateRSAKeyEncrypted, bytes16[] memory ivs, uint256[] memory blockNumbers);
-    function getAllKeys() external view returns (address[] memory owners, bytes[] memory publicRSAKeys, bytes[] memory privateRSAKeyEncrypted, bytes16[] memory ivs, uint256[] memory blockNumbers);
+    function addKey(string calldata publicKeyBase64, string calldata encryptedPrivateKeyBase64, string calldata ivBase64) external payable;
+    function getCurrentKey(address _address) external view returns (address owner, string memory publicKeyBase64, string memory encryptedPrivateKeyBase64, string memory ivBase64, uint256 blockNumber);
+    function getKeys(address _address) external view returns (Key[] memory);
+    function getAllKeys() external view returns (Key[] memory);
     function getAllKeysLength() external view returns (uint256);
     function getUserKeysLength(address _address) external view returns (uint256);
     function getDecryptionInstructions() external pure returns (string memory);
+}
+
+struct Key {
+    address owner;
+    string publicKeyBase64;
+    string encryptedPrivateKeyBase64;
+    string ivBase64;
+    uint256 blockNumber;
 }
