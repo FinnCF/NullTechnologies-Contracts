f7c97f03e9179c1a8bc22cb510d5c88e8f4ef073
Finn Casey Fierro <73711482+FinnCastro@users.noreply.github.com>
2023-08-19 19:16:34 +0100

    Small Fixes

--- contracts/IsnessKeyV1/IsnessKeyV1.sol
+++ contracts/IsnessKeyV1/IsnessKeyV1.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERCKMS1 {
+contract IsnessKeyV1 {
     // Struct representing a cryptographic key.
     struct Key {
         address owner;
@@ -69,11 +69,11 @@ contract ERCKMS1 {
     function getDecryptionInstructions() public pure returns (string memory) {
         return (
             "ENCRYPTION PROCESS:\n"
-            "1) Key Pair Generation: A public-private key pair is generated using the RSASSA-PKCS1-v1_5 algorithm. A 4096-bit modulus and a public exponent of [1, 0, 1] are used, and the hash algorithm is SHA-256. Keys are then encoded.\n"
+            "1) Key Pair Generation: A public-private key pair is generated using the RSASSA-PKCS1-v1_5 algorithm. A 4096-bit modulus and a public exponent of [1, 0, 1] are used, and the hash algorithm is SHA-256.\n"
             "2) Signature Creation: The owner of the key creates a signature by signing the public RSA key with their private Ethereum key.\n"
-            "3) Signature Hashing: The signature is hashed with the SHA-256 algorithm, resulting in a fixed-size hash value of 256bits.\n"
+            "3) Signature Hashing: The signature is hashed with the Keccak-256 algorithm, resulting in a fixed-size hash value of 256bits.\n"
             "4) Encryption of Private Key: Using the hashed signature as a secret key, the private RSA key is encrypted with AES-256. AES in Cipher Block Chaining (CBC) mode is used, and a random initialization vector (IV) is generated to ensure unique encryption.\n"
-            "5) Decryption of Private Key: To decrypt the encrypted private key, the owner recreates the hash by signing the public RSA key and hashing it with SHA-256 again. The encrypted private key is then decrypted using the hashed signature, IV and AES-CBC.\n"
+            "5) Decryption of Private Key: To decrypt the encrypted private key, the owner recreates the hash by signing the public RSA key and hashing it with Keccak-256 again. The encrypted private key is then decrypted using the hashed signature, IV and AES-CBC.\n"
             "This process ensures that only the owner, with the correct private Ethereum key, can derive the secret key needed for decryption. Standardized algorithms are used, enabling compatibility with various implementations. It is an extension of an ethereum private key to allow for the storage of RSA keys."
         );
     }
